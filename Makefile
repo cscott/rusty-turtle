074da0d437bf8bb8a30a0f66070402c5c692a7e7
C. Scott Ananian <cscott@cscott.net>
2013-05-19 12:03:06 -1000

    Add task-local interner; improve Makefile.

--- Makefile
+++ Makefile
@@ -14,3 +14,6 @@ test: intern-test op-test
 
 clean:
 	$(RM) main *-test
+
+# extra dependencies
+main: *.rs
--- intern.rs
+++ intern.rs
@@ -2,6 +2,7 @@
 // tries to do it "the rust way" which means the user can put the
 // interner wherever they want, can have multiple interners, etc.
 use core::hashmap::HashMap;
+use core::local_data::{local_data_get,local_data_set};
 
 pub struct IString {
     priv id : uint
@@ -21,7 +22,7 @@ impl IString {
     pub fn zero() -> IString { IString { id: 0 } }
 }
 
-struct Interner {
+priv struct Interner {
     priv map : @mut HashMap<~str, IString>,
     priv reverse_map : @mut ~[~str]
 }
@@ -53,6 +54,31 @@ impl Interner {
     pub fn get(&self, is : IString) -> ~str { is.to_str(self) }
 }
 
+// use a task-local interner
+priv fn interner_key(_x: @Interner) { }
+
+priv fn get_task_local_interner() -> @Interner {
+    unsafe {
+        let interner = match local_data_get(interner_key) {
+            Some(val) => val,
+            None => {
+                let data = @Interner::prefill(["__proto__"]);
+                local_data_set(interner_key, data);
+                data
+            }
+        };
+        interner
+    }
+}
+
+pub fn intern(s:&str) -> IString {
+    get_task_local_interner().intern(s)
+}
+
+pub fn intern_get(is:IString) -> ~str {
+    is.to_str(get_task_local_interner())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -64,6 +90,7 @@ mod tests {
     }
     #[test]
     fn i2 () {
+        // local interner
         let i = Interner::prefill(["__proto__"]);
         // first one is one:
         assert_eq!(i.intern (~"dog"), IString { id: 1 });
@@ -83,5 +110,27 @@ mod tests {
         assert_eq!(i.intern (~"__proto__"), IString::zero());
         // IString comparison
         assert_eq!(i.intern (~"rhino"), i.intern (~"rhino"));
+    }
+    #[test]
+    fn i3 () {
+        // task-global interner
+        // first one is one:
+        assert_eq!(intern (~"dog"), IString { id: 1 });
+        // re-use gets the same entry:
+        assert_eq!(intern (~"dog"), IString { id: 1 });
+        // different string gets a different #:
+        assert_eq!(intern (~"cat"), IString { id: 2 });
+        assert_eq!(intern (~"cat"), IString { id: 2 });
+        // dog is still at one
+        assert_eq!(intern (~"dog"), IString { id: 1 });
+        // new string gets 3
+        assert_eq!(intern (~"zebra" ), IString { id: 3 });
+        assert_eq!(intern_get(IString { id: 1 }), ~"dog");
+        assert_eq!(intern_get(IString { id: 2 }), ~"cat");
+        assert_eq!(intern_get(IString { id: 3 }), ~"zebra");
+        // __proto__ is zero
+        assert_eq!(intern (~"__proto__"), IString::zero());
+        // IString comparison
+        assert_eq!(intern (~"rhino"), intern (~"rhino"));
     }
 }
